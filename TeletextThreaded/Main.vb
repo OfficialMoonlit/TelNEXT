44c6e6a040bcaeb2c0ca004d0dc4d2a4e44c6277
Moonlit <mltgit@live.co.uk>
2012-12-02 13:32:51 +0000

    Version 1.5

---
+++ TeletextThreaded/Main.vb
@@ -0,0 +1,220 @@
+﻿'//// Imports
+Imports System.Net.Sockets
+Imports System.Text
+Imports System.Threading
+'//// End Imports
+
+Module Main
+
+    'Dim subPage As Integer
+    Dim multiPage As Boolean
+
+    Public subPageData As New subPage()
+
+    Class subPage
+        Public subPageNumber As Integer = 1
+        Public isSubPage As Boolean = False
+        Public subPageCount As Integer = 0
+        Public subPageWait As Integer = 0
+    End Class
+
+    Sub Main()
+        '//// Main program.
+
+
+        '//// Parse command line args.
+        Dim Port As Integer = Nothing
+
+        'If Command() <> Nothing Then '//// No command line arguments? Skip this.
+        'Dim CLIArgs As Array
+        'CLIArgs = Split(Command, " ") '//// Split command line args into an array.
+        'For i = LBound(CLIArgs) To UBound(CLIArgs) '//// Check arguments.
+        ' Select Case LCase(CLIArgs(i))
+        '     Case "-p", "/p", "-port", "/port" '//// Argument: User wants a custom port number.
+        ' If i = UBound(CLIArgs) Then
+        ' '//// No value specified.
+        ' '//// User forgot to add a port number.
+        ' Report("No port specified, using standard port.") '//// DEBUG: Tell user they fucked up.
+        ' Port = 5500 '//// Set the port for them.
+        ' Else
+        ' '//// User specified something.
+        ' i = i + 1 '//// Check the next value.
+        ' End If
+        ' If Left(CLIArgs(i), 1) = "-" Or Left(CLIArgs(i), 1) = "/" Then
+        ' '//// User forgot to add a port number.
+        ' Report("Invalid port specified, using standard port.") '//// DEBUG: Tell user they fucked up.
+        ' Port = 5500 '//// Set the port for them.
+        ' Else
+        ' If Int(Val(CLIArgs(i))) > 0 And Int(Val(CLIArgs(i))) < 65535 Then '//// Make sure user's port suggestion is valid.
+        ' Port = CLIArgs(i) '//// If it is, set the port number.
+        ' Else
+        ' Port = 5500 '//// If it isn't, set the port for them.
+        ' End If
+        ' End If
+        '     Case "-?", "/?", "-help", "/help", "-about", "/about" '//// Argument: User wants a custom port number.
+        ' Report(vbCrLf & "TNXT-P 1.0 by Moonlit.")
+        ' Report(vbCrLf & "Command line arguments:")
+        ' Report("Help (this): -? or -help")
+        ' Report("Custom port number: -p [port]")
+        ' End
+        ' End Select
+        ' Next
+        'Else
+        Port = 5500 '//// If a custom port wasn't supplied, use the default one.
+        'End If
+
+        '//// Variables
+        Dim serverSocket As New TcpListener(Port) '//// Depreciated method.
+        Dim clientSocket As TcpClient
+        Dim counter As Integer
+        '//// End Variables
+
+        serverSocket.Start() '//// Start listening.
+        Console.Clear() '//// DEBUG: Clear the console.
+        Report("TNXT 1.5 running on " & Environment.OSVersion.ToString & ".") '//// DEBUG: Tell user we're listening.
+        Report("Listening on port " & Port & ". Ready.")
+        counter = 0 '//// No clients.
+
+        While (True) '//// Loop to wait for new clients.
+            counter += 1 '//// Increase counter, we have a new client.
+            clientSocket = serverSocket.AcceptTcpClient() '//// Accept the connection.
+            Report("Client Connected: " & Convert.ToString(counter) & "") '//// DEBUG: Tell user we have a new client.
+            Dim client As New handleClient '//// New instance of handleClient to deal with the new client.
+            client.startClient(clientSocket, Convert.ToString(counter)) '//// Start client code.
+            Thread.Sleep(50)
+        End While
+
+        clientSocket.Close() '//// Close client socket(s).
+        serverSocket.Stop() '//// Stop thread(s).
+        Report("Quitting.") '//// DEBUG: Tell user we're quitting.
+        Console.ReadLine()
+        '//// End of program.
+    End Sub
+
+    Sub Report(ByVal msg As String)
+        '//// Send messages to the console.
+        Console.WriteLine(msg) '//// DEBUG: Tell user whatever's in the msg variable.
+    End Sub
+
+    Public Class handleClient
+        '//// Client thread code.
+
+        '//// Variables
+        Dim clientSocket As TcpClient
+        Dim clNo As String
+        '//// End Variables
+
+        Public Sub startClient(ByVal inClientSocket As TcpClient, ByVal clineNo As String)
+            '//// Handle new client.
+            Me.clientSocket = inClientSocket
+            Me.clNo = clineNo '//// Set client number.
+            Dim ClientThread As Thread = New Thread(AddressOf ClientLoop) '//// Create new client thread. 
+            ClientThread.Start() '//// Run new thread.
+        End Sub
+
+        Private Sub ClientLoop()
+            '//// Main code for each client.
+
+            '//// Variables
+            Dim ReceiveData(127) As Byte
+            Dim PageRequest As String = "100"
+            Dim PageData As String
+            Dim SendBytes As Byte()
+            Dim serverResponse As String
+            Dim networkStream As NetworkStream = clientSocket.GetStream()
+            '//// End Variables
+
+            Dim ClockThread As Thread = New Thread(AddressOf Update) '//// Create new clock update thread.
+            ClockThread.Start() '//// Run new thread.
+
+            serverResponse = AssemblePage(PageRequest) '//// Generate initial page.
+            SendBytes = Encoding.Unicode.GetBytes(serverResponse)
+            networkStream.Write(SendBytes, 0, SendBytes.Length) '//// Send page.
+            networkStream.Flush() '/// Flush the send queue.
+
+            While (True)
+                Try
+                    '//// Try the following:
+                    'Report("")
+                    networkStream.Read(ReceiveData, 0, 127) '//// Yay, new data.
+                    PageRequest = System.Text.Encoding.Default.GetString(ReceiveData) '//// Get new page request.
+                    PageRequest = Int(Val(Strings.Left(PageRequest, 3))) '//// Grab the numeric value of the first 3 characters.
+                    networkStream.Flush()
+
+                    If PageRequest >= 100 And PageRequest <= 999 Then '//// Page number too low, send page 100. Could be used to point to howto page.
+                        Report("Page Requested: Client " & clNo & " requested page " & PageRequest & ".") '//// DEBUG: Tell user a page has been requested and by which client.
+                        PageData = AssemblePage(PageRequest)
+                        serverResponse = PageData '//// Generate requested page.
+                        SendBytes = Encoding.Unicode.GetBytes(serverResponse)
+                        networkStream.Write(SendBytes, 0, SendBytes.Length) '//// Send page.
+                        networkStream.Flush() '//// Flush send queue.
+                        PageRequest = Nothing
+                    End If
+
+                    '//// If something went wrong:
+                Catch ex As Exception
+                    '//// Something went wrong, the client probably quit.
+                    Report("Client Disconnected: " & Me.clNo & ". Killing Page Thread.") '//// DEBUG: Tell user client has disconnected.
+                    Exit Sub
+                    Report("Client Disconnected: " & Me.clNo & ". End Page Thread.")
+                    Thread.CurrentThread.Abort() '//// Abort the thread.
+                    Report("Client Disconnected: " & Me.clNo & ". Killed Page Thread.")
+                End Try
+            End While
+
+        End Sub
+
+        Private Sub Update()
+            '//// Page update thread. 
+            '//// Variables
+            Dim SendBytes As Byte()
+            Dim serverResponse As String
+            Dim networkStream As NetworkStream = clientSocket.GetStream()
+            Dim pageData As String
+            '//// End Variables
+            While True
+                Try
+                    '//// Try the following:
+
+
+                    '//// EXPERIMENTAL SUBPAGE CODE
+                    SyncLock subPageData
+                        If subPageData.isSubPage = True Then
+                            If subPageData.subPageWait = 10 Then
+                                subPageData.subPageWait = 0
+                                If subPageData.subPageNumber > subPageData.subPageCount Then subPageData.subPageNumber = 1
+                                If subPageData.subPageNumber <= 0 Then subPageData.subPageNumber = 1
+                                'Report("Subpage Requested: Client " & clNo & " requested page " & subPageData.subPageNumber & ".") '//// DEBUG: Tell user a page has been requested and by which client.
+                                pageData = AssemblePage(subPageData.subPageNumber)
+                                serverResponse = pageData '//// Generate requested page.
+                                SendBytes = Encoding.Unicode.GetBytes(serverResponse)
+                                networkStream.Write(SendBytes, 0, SendBytes.Length) '//// Send page.
+                                networkStream.Flush() '//// Flush send queue.
+                                subPageData.subPageNumber = subPageData.subPageNumber + 1
+                            End If
+                            subPageData.subPageWait = subPageData.subPageWait + 1
+                        End If
+                    End SyncLock
+                    '//// EXPERIMENTAL SUBPAGE CODE
+
+                    serverResponse = RefreshClock() '//// Generate new clock data.
+                    SendBytes = Encoding.Unicode.GetBytes(serverResponse)
+                    networkStream.Write(SendBytes, 0, SendBytes.Length) '//// Send the new clock data.
+                    networkStream.Flush() '//// Flush the send queue.
+                    Thread.Sleep(1000) '//// Wait 1 second.
+
+                    '//// If something went wrong:
+                Catch ex As Exception
+                    '//// Something went wrong, the client probably quit.
+                    Report(ex.ToString)
+
+                    Report("Client Disconnected: " & Me.clNo & ". Killing Clock Thread.") '//// DEBUG: Tell user client has disconnected.
+                    Exit Sub
+                    Report("Client Disconnected: " & Me.clNo & ". End Clock Thread.")
+                    Thread.CurrentThread.Abort() '//// Abort the thread.
+                    Report("Client Disconnected: " & Me.clNo & ". Killed Clock Thread.")
+                End Try
+            End While
+        End Sub
+    End Class
+End Module
---
+++ TeletextThreaded/Page.vb
@@ -0,0 +1,160 @@
+﻿Imports System.Text
+
+Module Page
+
+    '//// Assign variables.
+    Dim CurrentPage As Integer
+    Dim Body As String '//// Variable for page data.
+    Dim FastText As String '//// Variable for FastText (coloured options) data.
+    Dim FirstLine As String '//// Variable for header data. Contains clock and page.
+    Dim FileNum As Integer '//// Variable for file access.
+
+    '//// Easy names for colour/formatting data.
+    Dim BGBlack As String = "[40m"
+    Dim BGDarkRed As String = "[41m" '//// Not used?
+    Dim BGDarkGreen As String = "[42m" '//// Not used?
+    Dim BGDarkYellow As String = "[43m" '//// Not used?
+    Dim BGDarkBlue As String = "[44m" '//// Not used?
+    Dim BGDarkPurple As String = "[45m" '//// Not used?
+    Dim BGDarkCyan As String = "[46m" '//// Not used?
+    Dim BGDarkWhite As String = "[47m" '//// Not used?
+    Dim BGReset As String = "[49m"
+    Dim BGGrey As String = "[100m"
+    Dim BGRed As String = "[101m"
+    Dim BGGreen As String = "[102m"
+    Dim BGYellow As String = "[103m"
+    Dim BGBlue As String = "[104m"
+    Dim BGPurple As String = "[105m"
+    Dim BGCyan As String = "[106m"
+    Dim BGWhite As String = "[107m"
+    Dim FGBlack As String = "[30m"
+    Dim FGDarkRed As String = "[31m" '//// Not used?
+    Dim FGDarkGreen As String = "[32m" '//// Not used?
+    Dim FGDarkYellow As String = "[33m" '//// Not used?
+    Dim FGDarkBlue As String = "[34m" '//// Not used?
+    Dim FGDarkPurple As String = "[35m" '//// Not used?
+    Dim FGDarkCyan As String = "[36m" '//// Not used?
+    Dim FGDarkWhite As String = "[37m" '//// Not used?
+    Dim FGReset As String = "[39m"
+    Dim FGGrey As String = "[90m"
+    Dim FGRed As String = "[91m"
+    Dim FGGreen As String = "[92m"
+    Dim FGYellow As String = "[93m"
+    Dim FGBlue As String = "[94m"
+    Dim FGPurple As String = "[95m"
+    Dim FGCyan As String = "[96m"
+    Dim FGWhite As String = "[97m"
+    '//// End of formatting data.
+
+    Public Function AssemblePage(ByVal NextPage As Integer)
+
+        If NextPage >= 100 Then CurrentPage = NextPage
+
+        SyncLock subPageData
+            If NextPage > 100 Then subPageData.subPageNumber = 1
+        End SyncLock
+
+        'Console.WriteLine("Generating page: " & CurrentPage) '//// DEBUG: Tell user page is being generated.
+
+        '//// Generate header.
+        FirstLine = BGReset & Chr(27) & "[1;1H" & Chr(27) & "[97;40m " & "P" & Format(CurrentPage, "000") & " " & Chr(27) & "[1;7H" & Chr(27) & "[22;30;107m" & "ChannelEM" & Chr(27) & "[97;40m" & " P" & Format(CurrentPage, "000") & " " & Format(Now, "ddd dd MMM") & Chr(27) & "[93m" & " " & Format(Now, "HH:mm/ss")
+        '//// Generate FastText buttons.
+        '//// FastTest disabled until compatibility fix. Replaced by version text temporarily. Fix this.
+        'FastText = BGReset & Chr(27) & "[25;1H" & Chr(27) & "[91;40m" & "   TEST   " & Chr(27) & "[92;40m" & "   PAGE   " & Chr(27) & "[93;40m" & "   FAST   " & Chr(27) & "[94;40m" & "   TEXT   "
+        FastText = BGReset & Chr(27) & "[97;40m" & CentreText("  TNXT Version 1.50 | 2012 | Moonlit", 25)
+
+        '//// Start page generation.
+        If NextPage = 999 Then '//// Check for reserved pages.
+            '//// Catch page 887, send custom page.
+            Body = Chr(27) & "[1;31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[0m[1;32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[0m[1;34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛÛ[34mÛ[31mÛ[32mÛ[34mÛ[31mÛ[0m"
+        ElseIf NextPage = 888 Then
+            '//// Catch page 888, send custom page.
+            Body = FGRed & BGBlack
+            Body = Body & CentreText("Reserved for subtitles.", 4) & vbCrLf
+        Else '//// If page isn't reserved, send normal page.
+            If My.Computer.FileSystem.DirectoryExists(CurrentPage) Then
+                'Report("IS A SUBPAGE")
+                SyncLock subPageData
+                    subPageData.isSubPage = True
+                    subPageData.subPageCount = System.IO.Directory.GetFiles(Environment.CurrentDirectory & "/" & CurrentPage).Length()
+                    Body = readFile(Environment.CurrentDirectory & "/" & CurrentPage & "/" & subPageData.subPageNumber & ".ans")
+                End SyncLock
+            Else
+                'Report("NOT A SUBPAGE")
+                subPageData.isSubPage = False
+                subPageData.subPageNumber = 0
+                If NextPage < 100 Then
+                    SyncLock subPageData
+                        Report(Environment.CurrentDirectory & "/" & CurrentPage & "/" & subPageData.subPageNumber & ".ans")
+                        Body = readFile(Environment.CurrentDirectory & "/" & CurrentPage & "/" & subPageData.subPageNumber & ".ans")
+                    End SyncLock
+                ElseIf My.Computer.FileSystem.FileExists(NextPage & ".ans") Then
+                    Body = readFile(NextPage & ".ans")
+                Else
+                    Body = readFile("1000.ans")
+                End If
+            End If
+        End If
+        '//// End page generation.
+
+        '//// Send page to main loop.
+        If InStr(UCase(Environment.OSVersion.ToString), "WINDOWS") Then
+            Return Chr(27) & "[2J" & Chr(27) & "[?25l" & FirstLine & vbCr & Body & FastText & Chr(27) & "[1;3H" & Chr(27) & "[0;1;32m"
+        Else
+            Return Chr(27) & "[2J" & Chr(27) & "[?25l" & FirstLine & vbLf & vbCr & Body & FastText & Chr(27) & "[1;3H" & Chr(27) & "[0;1;32m"
+        End If
+
+    End Function
+
+    Public Function CentreText(ByVal Text As String, ByVal Line As Integer)
+        '//// Add co-ords for centred text, send it back.
+        Return "[" & Line & ";" & Int(20 - (Len(Text) / 2)) & "H" & Text
+    End Function
+
+    Public Function RefreshClock()
+        '//// Refresh date/clock.
+        'Return BGReset & Chr(27) & "[s" & Chr(27) & "[1;21H" & Chr(27) & "[97;40m " & Format(Now, "ddd dd MMM") & Chr(27) & "[93m" & " " & subPage & Chr(27) & "[u"
+        Return BGReset & Chr(27) & "[s" & Chr(27) & "[1;21H" & Chr(27) & "[97;40m " & Format(Now, "ddd dd MMM") & Chr(27) & "[93m" & " " & Format(Now, "HH:mm/ss") & Chr(27) & "[u"
+    End Function
+
+    Public Function readFile(ByVal NextPage As String)
+
+        '//// Check for page, if exists send page <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        '//// Method 1: Windows
+        If InStr(UCase(Environment.OSVersion.ToString), "WINDOWS") Then
+            'Report("Windows file reading routine...")
+            FileNum = FreeFile()
+            NextPage = Replace(NextPage, "\", "/")
+            'Report(NextPage)
+            FileOpen(FileNum, NextPage, OpenMode.Input, OpenAccess.Read)
+            Body = ""
+            Do Until EOF(FileNum)
+                Body = Body & Environment.NewLine & LineInput(FileNum)
+            Loop
+            'If InStr(Body, Chr(10)) Then End
+            FileClose(FileNum)
+            Body = Replace(Body, Chr(10), Chr(10) & Chr(13))
+        Else
+            '//// Check for page, if exists send page <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+            '//// Method 2: Unix
+            'Report("*n?x file reading routine...")
+
+            Dim fileContents() As Byte = IO.File.ReadAllBytes(NextPage)
+            NextPage = Replace(NextPage, "\", "/")
+            'Report(NextPage)
+            Body = ""
+            For x = 0 To fileContents.Length - 1
+                'Report("Char: " & fileContents(x) & Environment.NewLine)
+                If Chr(fileContents(x)) = Environment.NewLine Then
+                    'Report("------------------E-NL FOUND")
+                    Body = Body & Environment.NewLine & vbCr
+                Else
+                    Body = Body & Chr(fileContents(x))
+                End If
+            Next
+            'Report("Page found...")
+        End If
+
+        Return Body
+    End Function
+End Module
